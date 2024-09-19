9668ee8bf3ba1069199000add3d4cb055de76465
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2024-09-19 11:37:29 +0530

    add: authenticator

---
+++ authentication-service/cmd/api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"authentication/data"
+
+	"github.com/IBM-Cloud/power-go-client/power/models"
+)
+
+
+
+const webPort = "80"
+
+type Config struct {
+	DB *sql.DB
+	models data.Models
+}
+
+func main(){
+
+
+
+}
